52a4657692ce12a3cde24433a54f92286d6b1dc4
taehun-kmu <taehun@kookmin.ac.kr>
2025-05-07 15:13:39 +0900

    fix nvim

--- script/nvim.sh
+++ script/nvim.sh
@@ -33,6 +33,11 @@ $SUDO $APT update && $SUDO $APT install -y --no-install-recommends git ninja-bui
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/taehun-kmu/auto/main/script/gcc.sh)"
 
 cd && git clone -b v0.11.0 --depth=1 https://github.com/neovim/neovim.git .neovim
-
 cd .neovim && make CMAKE_BUILD_TYPE=Release && $SUDO make install
+
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+
+git clone --depth 1 https://github.com/AstroNvim/template $HOME/.config/nvim
+rm -rf $HOME/.config/nvim/.git
+nvim
 
