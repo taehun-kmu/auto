6f11f3ac57e326d9f64d9abc54db3fe993ed9c67
taehun-kmu <taehun@kookmin.ac.kr>
2025-05-12 12:43:23 +0900

    feat(lang): Setup to node & npm

---
+++ script/node.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast || return 1
+  ! LANG= apt-fast -n -v 2>&1 | grep -q "may not run apt-fast"
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends curl zip unzip
+
+# fnm
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# Completions 
+echo 'eval "$(fnm completions --shell bash)"' >> $HOME/.bashrc
+
+# Installation
+fnm install --lts
+
+# node
+npm install -g npm@latest
+npm install -g neovim
