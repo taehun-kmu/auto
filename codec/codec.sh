f842aa5be558c8d6567ec3e35d4c2230b99f75ad
taehun-kmu <taehun@kookmin.ac.kr>
2025-05-12 14:27:42 +0900

    feat: Nvidia Video Codec SDK 12.2 setup

---
+++ codec/codec.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+$SDUO $APT update && $SUDO $APT install -y --no-install-recommends zip unzip
+
+wget -P . https://raw.githubusercontent.com/taehun-kmu/auto/main/codec/SDK_12.2.zip && unzip SDK_12.2.zip
+
+$SUDO cp Video_Codec_SDK_*/Interface/*.h /usr/local/cuda/targets/$(uname -m)-linux/include/
+$SUDO cp Video_Codec_SDK_*/Lib/linux/stubs/$(uname -m)/*.so /usr/local/cuda/lib64/
+
