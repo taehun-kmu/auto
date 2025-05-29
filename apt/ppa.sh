0a5f9cbf838aa0d1a3217867669ea51d2a7f421d
taehun-kmu <taehun@kookmin.ac.kr>
2025-05-29 13:59:23 +0900

    feat(apt): Setup to Ubuntu ppa

---
+++ apt/ppa.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast || return 1
+  ! LANG= apt-fast -n -v 2>&1 | grep -q "may not run apt-fast"
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Installation
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends software-properties-common gpg-agent
+
