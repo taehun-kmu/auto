e6d1859a69953a6163597c813a917fbc8125db34
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-26 19:36:39 +0900

    feat: Gihub CLI Setup

---
+++ script/gh.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Installation
+(type -p wget >/dev/null || ($SUDO $APT update && $SUDO $APT install --no-install-recommends -y wget)) \
+	&& $SUDO mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | $SUDO tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& $SUDO chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& $SUDO mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& $SUDO $APT update \
+	&& $SUDO $APT install -y gh
