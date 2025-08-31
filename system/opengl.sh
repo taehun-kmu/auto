dfc27d8b15cd86c700e858e5609488e14fbeadc1
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-01 01:44:26 +0900

    feat(graphic): Setup to OpenGL

---
+++ system/opengl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends mesa-utils vulkan-tools
+
+echo -e "\n$(glxinfo | grep 'OpenGL renderer')"
