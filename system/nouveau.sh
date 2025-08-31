761c4772fcbf24e59307df3bcb377d601b7aa3f4
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-01 01:31:31 +0900

    feat(graphic): Disalbe nouveau

---
+++ system/nouveau.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends wget
+
+# Setup
+$SUDO wget -P /etc/modprobe.d https://raw.githubusercontent.com/taehun-kmu/auto/main/system/conf/blacklist-nouveau.conf
