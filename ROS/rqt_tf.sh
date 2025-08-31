448bf39e9f5cf555efd6e80024e09b85a7824668
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-01 05:14:59 +0900

    feat: Setup to ros2 humble rqt tf tree

---
+++ ROS/rqt_tf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast || return 1
+  ! LANG= apt-fast -n -v 2>&1 | grep -q "may not run apt-fast"
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement 
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends ros-humble-tf2-tools ros-humble-rqt-tf-tree
+
+
+# Setup 
+source /opt/ros/humble/setup.bash
+
+rqt --clear-config
