6fcab13017e4c198b12c1ce6bfc0454183afe976
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-05 01:23:45 +0900

    feat(grub): Setup to Grub Customizer

---
+++ system/grub-custom.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends software-properties-common gpg-agent
+
+# Installation
+$SUDO add-apt-repository -y ppa:danielrichter2007/grub-customizer
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends grub-customizer
