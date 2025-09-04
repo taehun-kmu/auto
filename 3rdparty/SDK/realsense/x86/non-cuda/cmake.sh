2a3e436201eed0021b23b1a156d7ded51bd9ae04
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-05 05:11:53 +0900

    feat(sdk): Setup to librealsense with CUDA

---
+++ 3rdparty/SDK/realsense/x86/non-cuda/cmake.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+
+build_dir="$HOME/Workspace/3rdparty/SDK/librealsense"
+cd "$build_dir"
+
+
+# Installation
+cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -D BUILD_EXAMPLES=ON -D BUILD_WITH_OPENMP=ON -D BUILD_PYTHON_BINDINGS=ON
---
+++ 3rdparty/SDK/realsense/x86/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+
+# Requirement
+$SUDO $APT update && $SUDO $APT upgrade && $SUDO $APT dist-upgrade
+$SUDO $APT update && $SUDO $APT install -y --no-install-recommends libssl-dev libusb-1.0-0-dev libudev-dev \
+                                                                   pkg-config libgtk-3-dev \
+                                                                   git wget cmake build-essential \
+                                                                   libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev \
+                                                                   freeglut3-dev at v4l-utils
+
+
+# Repository
+realsense_dir="$HOME/Workspace/3rdparty/SDK"
+mkdir -p "$realsense_dir" && cd "$realsense_dir"
+git clone -b v2.56.4 https://github.com/IntelRealSense/librealsense.git && cd librealsense
+
+
+# udev
+./scripts/setup_udev_rules.sh
