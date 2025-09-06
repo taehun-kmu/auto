3c9514dceba1420cb3947ad0c899d736d1b65a15
taehun-kmu <taehun@kookmin.ac.kr>
2025-09-07 03:41:04 +0900

    feat(library): Setup to Multi-CUDA

---
+++ 3rdparty/nvidia/cuda/126.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+if user_can_sudo; then
+  SUDO="sudo"
+else
+  SUDO="" # To support docker environment
+fi
+
+user_can_apt() {
+  command_exists apt-fast
+}
+
+if user_can_apt; then
+  APT="apt-fast"
+else
+  APT="apt-get" # Basic command
+fi
+
+# Requirement
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/taehun-kmu/auto/main/script/gcc.sh)"
+
+# Download
+cuda_dir="$HOME/Downloads/CUDA"
+mkdir -p "$cuda_dir" && cd "$cuda_dir"
+wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
